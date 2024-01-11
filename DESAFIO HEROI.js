867cc5ff5960aff2d607faed4edcddb1bc974369
ArielOCruel <155584208+ArielOCruel@users.noreply.github.com>
2024-01-11 11:23:56 -0300

    Add files via upload

---
+++ DESAFIO HEROI.js
@@ -0,0 +1,35 @@
+let meuHeroi = ("Goku")
+let nivelXp = 10001.00   
+
+if(nivelXp < 1000.00){
+    console.log("O nome do herói é " +meuHeroi + " e o nível de experiência é Ferro " )
+
+}else if(nivelXp >1000.00 && nivelXp <= 2000.00 ){
+    console.log("O nome do herói é " +meuHeroi + " e o nível de experiência é Bronze " )
+
+}else if(nivelXp >= 2001.00 && nivelXp <= 5000.00 ){
+    console.log("O nome do herói é " +meuHeroi + " e o nível de experiência é Prata " )
+
+}else if(nivelXp >= 5001.00 && nivelXp <= 7000.00 ){
+    console.log("O nome do herói é " +meuHeroi + " e o nível de experiência é Ouro " )
+
+}else if(nivelXp >= 7001.00 && nivelXp <= 8000.00 ){
+    console.log("O nome do herói é " +meuHeroi + " e o nível de experiência é Platina " )
+
+}else if(nivelXp >= 8001.00 && nivelXp <= 9000.00 ){
+    console.log("O nome do herói é " +meuHeroi + " e o nível de experiência é Ascendente " )
+
+}else if(nivelXp >= 9001.00 && nivelXp <= 10000.00 ){
+    console.log("O nome do herói é " +meuHeroi + " e o nível de experiência é Imortal " )
+
+}else{
+    console.log("O nome do herói é " +meuHeroi + " e o nível de experiência é Radiante " )
+    
+}
+
+
+    
+
+
+
+
